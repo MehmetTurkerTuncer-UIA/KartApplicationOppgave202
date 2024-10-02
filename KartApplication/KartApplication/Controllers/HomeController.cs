e61f293f5b575af722a7b14950269226c1058f0f
MehmetTurkerTuncer-UIA <mehmettt@student.uia.no>
2024-10-02 11:12:55 +0200

    ProjectStarting

---
+++ KartApplication/KartApplication/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using KartApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace KartApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult SubmitForm(FormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Form verilerini işle
+                return RedirectToAction("FormResult", model);
+            }
+            return View("Index", model);
+        }
+
+        public IActionResult FormResult(FormModel model)
+        {
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
---
+++ KartApplication/KartApplication/Models/FormModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class FormModel
+{
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    public string Email { get; set; }
+}
+
